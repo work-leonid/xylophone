83effa2e615df10e40eda4d2eeae977089034ff2
leonid nazarov <work.leonid@gmail.com>
2021-01-26 09:56:45 +0300

    Init empty xcode project
    Create new project

---
+++ Xylophone/Xylophone/XylophoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneApp.swift
+//  Xylophone
+//
+//  Created by Leonid on 26.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct XylophoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
