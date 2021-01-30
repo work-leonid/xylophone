e83e892a16e1a2020127078eb5473103260b5564
leonid nazarov <work.leonid@gmail.com>
2021-01-30 17:02:59 +0300

    start to add animations between views

--- Xylophone/Xylophone/ContentView.swift
+++ Xylophone/Xylophone/ContentView.swift
@@ -11,45 +11,79 @@ import AVFoundation
 struct ContentView: View {
     let items = xylphonesItems
     @State private var soundPlayer: AVAudioPlayer?
+    @State private var fullScreen = false
     
     @Environment(\.verticalSizeClass) var sizeClass: UserInterfaceSizeClass?
 
     
     var body: some View {
-        if sizeClass == .compact {
-            HStack(spacing: 0) {
-                ForEach(items) {item in
-                    Button(action: {
-                        playSound(sound: item.title)
-                    }) {
-                        item.color
-                            .edgesIgnoringSafeArea(.all)
-                    }
+        ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
+            ZStack {
+                if fullScreen {
+                    FullScreenViewAnimation()
+//                    fullScreenView
+                } else {
+//                    separateButtonsView
+                    NotFullScreenViewAnimation()
+                }
+            }
+            .transition(.move(edge: .bottom))
+            .animation(.spring())
+            
+            
+            
+            Button(action: {
+                withAnimation {
+                    self.fullScreen.toggle()
+                }
+            }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(Color.blue.clipShape(Circle()))
+                    .shadow(radius: fullScreen ? 10 : 0)
+                    .padding()
+                    .rotationEffect(Angle(degrees: fullScreen ? 360 : 0))
+                    .scaleEffect(fullScreen ? 1.3 : 1)
+                    .animation(.spring())
                     
-                }
             }
-        } else {
-            VStack(spacing: 10) {
-                ForEach(items.indices) { item in
-                    GeometryReader { geo in
-                        Button(action: {
-                            playSound(sound: items[item].title)
-//                            print(items[0])
-                        }) {
-                            items[item].color
-                                .cornerRadius(14)
-                                .frame(width: geo.size.width - CGFloat(item + 1) * 10, height: geo.size.height)
-                                .position(x: geo.size.width / 2, y: geo.size.height / 2)
-                                
-//                                Text("\(geo.size.height) and \(item + 1)")
-                            
-                        }
-                    }
-                }
-            }
-//                                .edgesIgnoringSafeArea(.all)
         }
         
+    }
+    
+    private var fullScreenView: some View {
+        VStack(spacing: 10) {
+            ForEach(items.indices) { item in
+                GeometryReader { geo in
+                    Button(action: {
+                        playSound(sound: items[item].title)
+                    }) {
+                        items[item].color
+                            .cornerRadius(14)
+                            .frame(width: geo.size.width - CGFloat(item + 1) * 10, height: geo.size.height)
+                            .position(x: geo.size.width / 2, y: geo.size.height / 2)
+//                            .transition(.slide)
+//                            .animation(.ripple(index: item))
+                    }
+                }
+            }
+        }
+    }
+    
+    private var separateButtonsView: some View {
+        VStack(spacing: 0) {
+            ForEach(items.indices) { item in
+                    Button(action: {
+                        playSound(sound: items[item].title)
+                    }) {
+                        items[item].color
+//                            .transition(.slide)
+//                            .animation(.ripple(index: item))
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
     
     func playSound(sound: String) {
@@ -63,7 +97,35 @@ struct ContentView: View {
             print("error")
         }
     }
+    
+    
 }
+
+struct FullScreenViewAnimation: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.green)
+            .frame(width: 100, height: 100)
+    }
+}
+
+struct NotFullScreenViewAnimation: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.red)
+            .frame(width: 200, height: 500)
+    }
+}
+
+
+//extension Animation {
+//    static func ripple(index: Int) -> Animation {
+//        Animation.spring(dampingFraction: 0.5)
+//            .speed(2)
+//            .delay(0.03 * Double(index))
+//    }
+//
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
