b1502b80c2f9603f75fbb2e9f3b0e4d36bdd553e
leonid nazarov <work.leonid@gmail.com>
2021-01-30 11:46:15 +0300

    Add play sound logic

--- Xylophone/Xylophone/ContentView.swift
+++ Xylophone/Xylophone/ContentView.swift
@@ -6,20 +6,34 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     let items = xylphonesItems
+    @State private var soundPlayer: AVAudioPlayer?
     
     var body: some View {
         VStack(spacing: 0) {
             ForEach(items) {item in
                 Button(action: {
-                    print(item.title)
+                    playSound(sound: item.title)
                 }) {
                     item.color
                         .edgesIgnoringSafeArea(.all)
                 }
             }
+        }
+    }
+    
+    func playSound(sound: String) {
+        let path = Bundle.main.path(forResource: sound, ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.play()
+        } catch {
+            print("error")
         }
     }
 }
