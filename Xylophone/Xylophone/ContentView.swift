5482c2bf555b22bdc389dbbab95318e8415ba563
leonid nazarov <work.leonid@gmail.com>
2021-01-30 21:38:20 +0300

    change layout animation

--- Xylophone/Xylophone/ContentView.swift
+++ Xylophone/Xylophone/ContentView.swift
@@ -1,60 +1,59 @@
 //
-//  ContentView.swift
+//  SwiftUIView.swift
 //  Xylophone
 //
-//  Created by Leonid on 26.01.2021.
+//  Created by Leonid on 30.01.2021.
 //
 
 import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-    let items = xylphonesItems
-    @State private var soundPlayer: AVAudioPlayer?
-    @State private var fullScreen = false
+    let keys = xylphonesItems
     
-    @Environment(\.verticalSizeClass) var sizeClass: UserInterfaceSizeClass?
-
+    @State private var soundPlayer: AVAudioPlayer?
+    @State private var isFullScreen = true
     
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
-            ZStack {
-                if fullScreen {
-                    FullScreenViewAnimation()
-                        .transition(.moveAndFade)
-                } else {
-                    NotFullScreenViewAnimation()
-                        .transition(.moveAndFade)
+            VStack(spacing: isFullScreen ? 0 : 15) {
+                ForEach(keys.indices) { index in
+                    GeometryReader { geo in
+                        Button(action: {
+                            playSound(byName: keys[index].title)
+                        }) {
+                            keys[index].color
+                                .cornerRadius(isFullScreen ? 0 : 15)
+                                .frame(
+                                    width: isFullScreen ? geo.size.width : geo.size.width - CGFloat(index + 1) * 14,
+                                       height: geo.size.height)
+                                .position(x: geo.size.width / 2, y: geo.size.height / 2)
+                                .animation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0.4))
+                        }
+                    }
                 }
             }
-            .animation(.spring())
-            
-            
+            .edgesIgnoringSafeArea(isFullScreen ? .all : .horizontal)
             
             Button(action: {
-                withAnimation {
-                    self.fullScreen.toggle()
-                }
+                isFullScreen.toggle()
             }) {
-                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .background(Color.blue.clipShape(Circle()))
-                    .shadow(radius: fullScreen ? 10 : 0)
+                Image(systemName: isFullScreen ? "arrow.up.backward.and.arrow.down.forward.circle.fill" : "arrow.down.right.and.arrow.up.left.circle.fill")
+                    .font(.largeTitle)
+                    .background(Color.white)
+                    .foregroundColor(Color.pink)
+                    .clipShape(Circle())
                     .padding()
-                    .rotationEffect(Angle(degrees: fullScreen ? 360 : 0))
-                    .scaleEffect(fullScreen ? 1.3 : 1)
                     .animation(.spring())
                     
             }
         }
-        
     }
     
-    func playSound(sound: String) {
-        let path = Bundle.main.path(forResource: sound, ofType: "wav")!
+    func playSound(byName: String) {
+        let path = Bundle.main.path(forResource: byName, ofType: "wav")!
         let url = URL(fileURLWithPath: path)
-    
+        
         do {
             soundPlayer = try AVAudioPlayer(contentsOf: url)
             soundPlayer?.play()
@@ -64,76 +63,8 @@ struct ContentView: View {
     }
 }
 
-
-
-struct FullScreenViewAnimation: View {
-    let items = xylphonesItems
-//    var action: (() -> Void)?
-    
-    var body: some View {
-        VStack(spacing: 10) {
-            ForEach(items.indices) { item in
-                GeometryReader { geo in
-                    Button(action: {
-//                        playSound(sound: items[item].title)
-                    }) {
-                        items[item].color
-                            .cornerRadius(14)
-                            .frame(width: geo.size.width - CGFloat(item + 1) * 10, height: geo.size.height)
-                            .position(x: geo.size.width / 2, y: geo.size.height / 2)
-//                            .transition(.move(edge: .trailing))
-                            .animation(.ripple(index: item))
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct NotFullScreenViewAnimation: View {
-    let items = xylphonesItems
-    
-    var body: some View {
-        VStack(spacing: 0) {
-            ForEach(items.indices) { item in
-                    Button(action: {
-//                        playSound(sound: items[item].title)
-                    }) {
-                        items[item].color
-//                            .transition(.move(edge: .trailing))
-                            .animation(.ripple(index: item))
-                }
-            }
-        }
-        .edgesIgnoringSafeArea(.all)
-    }
-}
-
-
-extension Animation {
-    static func ripple(index: Int) -> Animation {
-        Animation.spring(dampingFraction: 0.2)
-            .speed(2)
-            .delay(0.03 * Double(index))
-    }
-
-}
-
-extension AnyTransition {
-    static var moveAndFade: AnyTransition {
-        let insertion = AnyTransition.move(edge: .trailing)
-            .combined(with: .opacity)
-        let removal = AnyTransition.scale
-            .combined(with: .opacity)
-        return .asymmetric(insertion: insertion, removal: removal)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
+struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ContentView()
-                
-        }
+        ContentView()
     }
 }
